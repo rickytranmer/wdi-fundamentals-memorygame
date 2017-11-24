f4f0a86ea830bc8a9a7df827b3864083e5364f6f
Ricky Tranmer <rickytranmer@Rickys-MacBook-Pro.local>
2017-11-24 08:33:06 -0700

    updated game logic using DOM

--- js/main.js
+++ js/main.js
@@ -33,13 +33,24 @@ var checkForMatch = function() {
 		}
 	}
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage)
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("-" + cards[cardId].suit);
 	console.log("-" + cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
+var gameBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
-flipCard(0);
-flipCard(2);
+gameBoard();
+
